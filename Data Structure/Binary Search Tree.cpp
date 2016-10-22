b7f74c825bebca0e4e469a721dcda72bfaab5c2c
Will-MingLun-Li <will.liatw@gmail.com>
2016-10-21 23:48:50 -0400

    Setting up BST

---
+++ Data Structure/Binary Search Tree.cpp
@@ -0,0 +1,20 @@
+// Binary Search Tree
+// Insertion
+// Deletion
+// Access ??
+// Search ??
+
+#include <iostream>
+using namespace std;
+
+struct node {
+	int data;
+	int *left;
+	int *right;
+};
+
+int main() {
+
+	system("pause");
+	return 0;
+}
