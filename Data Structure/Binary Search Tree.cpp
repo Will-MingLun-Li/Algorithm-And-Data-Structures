be35f7bdb92e53f5ec2ec13727809848ebe89d9c
Will-MingLun-Li <will.liatw@gmail.com>
2016-10-22 22:35:05 -0400

    Added traversals and destroy

--- Data Structure/Binary Search Tree.cpp
+++ Data Structure/Binary Search Tree.cpp
@@ -1,8 +1,8 @@
 // Binary Search Tree
 // Insertion: 
 // Deletion: 
-// Destroy:
-// Traversal:
+// Destroy BST: O(n)
+// Traversals: O(n)
 
 #include <iostream>
 using namespace std;
@@ -41,6 +41,42 @@ bst::~bst() {
 
 void bst::destroyBST() {
 	destroyBST(root);
+}
+
+void bst::destroyBST(node *node) {
+	if (node != NULL) {
+		destroyBST(node->left);
+		destroyBST(node->right);
+
+		delete node;
+	}
+}
+
+void bst::preorder(node *node) {
+	if (node == NULL)
+		return;
+
+	cout << node->data << " ";
+	preorder(node->left);
+	preorder(node->right);
+}
+
+void bst::inorder(node *node) {
+	if (node == NULL)
+		return;
+
+	inorder(node->left);
+	cout << node->data << " ";
+	inorder(node->right);
+}
+
+void bst::postorder(node *node) {
+	if (node == NULL)
+		return;
+
+	postorder(node->left);
+	postorder(node->right);
+	cout << node->data << " ";
 }
 
 int main() {
