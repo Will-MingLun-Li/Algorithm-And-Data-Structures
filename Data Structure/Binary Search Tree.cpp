4f7b9b740b479486e7820c568e0edee9ee7c3d7f
Will-MingLun-Li <will.liatw@gmail.com>
2016-10-22 21:04:05 -0400

    More setup stuff

--- Data Structure/Binary Search Tree.cpp
+++ Data Structure/Binary Search Tree.cpp
@@ -1,20 +1,49 @@
 // Binary Search Tree
-// Insertion
-// Deletion
-// Access ??
-// Search ??
+// Insertion: 
+// Deletion: 
+// Destroy:
+// Traversal:
 
 #include <iostream>
 using namespace std;
 
 struct node {
 	int data;
-	int *left;
-	int *right;
+	node *left;
+	node *right;
 };
+
+class bst {
+	public:
+		bst();
+		~bst();
+
+		void insert(int key);
+		void remove(int key);
+		void destroyBST();
+		void preorder(node *node);
+		void inorder(node *node);
+		void postorder(node *node);
+
+	private:
+		node *root;
+
+		void destroyBST(node *node);
+};
+
+bst::bst() {
+	root = NULL;
+}
+
+bst::~bst() {
+	destroyBST();
+}
+
+void bst::destroyBST() {
+	destroyBST(root);
+}
 
 int main() {
 
-	system("pause");
 	return 0;
 }
