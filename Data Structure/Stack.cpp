5d6239127ee702a1a834b3aff2dfa303a0899170
Will-MingLun-Li <will.liatw@gmail.com>
2016-10-31 21:53:17 -0400

    Finished stack

--- Data Structure/Stack.cpp
+++ Data Structure/Stack.cpp
@@ -13,17 +13,94 @@ struct node {
 
 template <typename T>
 class stack {
-	public:
-		stack();
-		~stack();
+public:
+	stack();
+	~stack();
 
-		void push();
-		void pop();
-		
-	private:
+	void push(T val);
+	T pop();
+	bool isEmpty();
+	void print();
+
+private:
+	node<T> *topNode;
+	int size;
 };
 
-int main () {
+template <typename T>
+stack<T>::stack() {
+	topNode = NULL;
+	size = 0;
+}
 
-	return 0;
+template <typename T>
+stack<T>::~stack() {
+	while (!isEmpty()) {
+		pop();
+	}
+}
+
+template <typename T>
+void stack<T>::push(T val) {
+	if (size == 0) {
+		node<T> *newNode = new node<T>();
+		newNode->data = val;
+		topNode = newNode;
+	}
+	else {
+		node<T> *newNode = new node<T>();
+		newNode->data = val;
+		newNode->next = topNode;
+		topNode = newNode;
+	}
+
+	size++;
+}
+
+template <typename T>
+T stack<T>::pop() {
+	if (!isEmpty()) {
+		node <T> *temp = topNode;
+		T poppedData = topNode->data;
+		topNode = topNode->next;
+		delete temp;
+		size--;
+
+		return poppedData;
+	}
+}
+
+template <typename T>
+bool stack<T>::isEmpty() {
+	return size == 0;
+}
+
+template <typename T>
+void stack<T>::print() {
+	node<T> *temp = topNode;
+	while (temp != NULL) {
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+	delete temp;
+}
+
+
+int main() {
+	stack<int> s;
+	s.push(3);
+	s.push(1);
+	s.push(4);
+	s.push(1);
+	s.push(5);
+	s.print();
+	s.pop();
+	s.print();
+	s.pop();
+	s.print();
+	s.pop();
+	s.print();
+
+	system("pause");
 }
